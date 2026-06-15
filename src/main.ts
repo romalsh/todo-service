060afb7bb278cd53130ce709b8c7734530566274
roman <roman>
2026-06-15 19:51:27 +0500

    chore: configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AllExceptionsFilter, LoggingInterceptor } from '@libs/common';
 import { AppModule } from './app.module';
@@ -25,8 +26,18 @@ async function bootstrap(): Promise<void> {
 	app.useGlobalFilters(new AllExceptionsFilter());
 	app.useGlobalInterceptors(new LoggingInterceptor());
 
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle('To-Do Service API')
+		.setDescription('Secure REST API for a mobile to-do application')
+		.setVersion('1.0')
+		.addBearerAuth()
+		.build();
+	const document = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('docs', app, document);
+
 	const port = config.get('port', { infer: true });
 	await app.listen(port);
+	Logger.log(`Running on http://localhost:${port} (docs at /docs)`, 'Bootstrap');
 }
 
 bootstrap().catch((err) => {
