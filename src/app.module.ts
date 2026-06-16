e57b2b1fcbe90f696e68436037ef9051bda39d48
roman <roman>
2026-06-16 12:21:52 +0500

    feat(app): register users, auth and tasks feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { CoreModule } from './core/core.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-	imports: [CoreModule],
+	imports: [CoreModule, UsersModule, AuthModule, TasksModule],
 	providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
