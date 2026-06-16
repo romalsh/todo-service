62683de4892145c25ddf944f929cac8325302159
roman <roman>
2026-06-16 14:41:09 +0500

    chore: added prettier

--- tests/globalTeardown.ts
+++ tests/globalTeardown.ts
@@ -1,9 +1,8 @@
 import type { StartedPostgreSqlContainer } from '@testcontainers/postgresql';
 
 export default async function globalTeardown(): Promise<void> {
-	const container = (
-		globalThis as { __PG_CONTAINER__?: StartedPostgreSqlContainer }
-	).__PG_CONTAINER__;
+	const container = (globalThis as { __PG_CONTAINER__?: StartedPostgreSqlContainer })
+		.__PG_CONTAINER__;
 	if (container) {
 		await container.stop();
 	}
